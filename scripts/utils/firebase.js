b4f159d63239bdf38b534ae1b89e1a1853159965
abhaysoni-qait <abhaysoni@qainfotech.com>
2019-01-31 18:56:41 +0530

    1st commit mvc based

---
+++ scripts/utils/firebase.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUSWN1W7xtVTlOTwgc8XzyD9k40LtS4EM",
+    authDomain: "book-search-engine-proto.firebaseapp.com",
+    databaseURL: "https://book-search-engine-proto.firebaseio.com",
+    projectId: "book-search-engine-proto",
+    storageBucket: "book-search-engine-proto.appspot.com",
+    messagingSenderId: "810572379147"
+  };
+  firebase.initializeApp(config);
